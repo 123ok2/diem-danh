baea16c9987f1c7646265bf5b58e496eb0d01653
DUY CONG HANH <134817450+123ok2@users.noreply.github.com>
2025-12-04 23:35:29 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -143,7 +143,7 @@ const AuthScreen = () => {
               </div>
            </div>
 
-           <h1 className="text-2xl font-blUE text-slate-800 tracking-tight">{isLogin ? 'PTDTBT THCS THU CÚC' : 'Tạo Tài Khoản'}</h1>
+           <h1 className="text-2xl font-black text-slate-800 tracking-tight">{isLogin ? 'PTDTBT THCS THU CÚC' : 'Tạo Tài Khoản'}</h1>
 
 <p className="text-slate-500 text-sm font-medium">Hệ thống điểm danh thông minh</p>
    <p className="text-slate-500 text-xs mt-1">
